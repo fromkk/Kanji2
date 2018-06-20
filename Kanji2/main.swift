4ecaa841af6f05c6e5c37f5aae9aa3c3c0aa2612
Kazuya Ueoka <info@fromkk.info>
2018-06-20 18:05:04 +0900

    Initial Commit

---
+++ Kanji2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Kanji2
+//
+//  Created by Kazuya Ueoka on 2018/06/20.
+//  Copyright © 2018 fromKK. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
